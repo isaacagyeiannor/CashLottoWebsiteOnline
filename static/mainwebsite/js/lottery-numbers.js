66e00ddbfcc692dcf987b9eb9c0dcf61000860d7
Isaac Agyei Annor <51149205+isaacagyeiannor@users.noreply.github.com>
2022-12-15 23:16:14 +0000

    lottery logic done

--- static/mainwebsite/js/lottery-numbers.js
+++ static/mainwebsite/js/lottery-numbers.js
@@ -1,6 +1,14 @@
 
 
 //Show or hide lottery 
+
+$( "#lottery_balls li img" ).unbind().on( "click", function(e) {
+  if( ($("select").find(":selected").val()) == ''){
+    alert('Select lottery type');
+  }
+
+});
+
 $("select").on("change", function() {
 	var lottery_type = $("select").find(":selected").val();
   let numberOfSelection = 0;
@@ -10,21 +18,10 @@ switch (lottery_type) {
     $("#selected_numbers li").remove();
     $("#lottery_selected_text h3").remove();
     $("#lottery_selected_text").prepend(' <h3>You can select only 2 numbers</h3>');
-    
 
     //function
-    $( "#lottery_balls li img" ).unbind().on( "click", function(e) {
-      if( ($("#selected_numbers li").length) < numberOfSelection){
-        var altText = e.target.alt;
-        $("#selected_numbers").append('<li>'+altText+'</li>');
-        Event.preventDefault();
-      }else {
-        alert("Selection full")
-      }
-
-      
-
-    });
+    LotterySelection()
+    
     break;
 
   case "3 Direct":
@@ -34,17 +31,7 @@ switch (lottery_type) {
     $("#lottery_selected_text").prepend(' <h3>You can select only 3 numbers</h3>');
 
     //function
-    $( "#lottery_balls li img" ).unbind().on( "click", function(e) {
-      if( ($("#selected_numbers li").length) < numberOfSelection){
-        var altText = e.target.alt;
-        $("#selected_numbers").append('<li>'+altText+'</li>');
-        Event.preventDefault();
-      }else {
-        alert("Selection full")
-      }
-
-    });
-    
+    LotterySelection();
     break;
 
   case "Perm 2":
@@ -54,15 +41,7 @@ switch (lottery_type) {
     $("#lottery_selected_text").prepend(' <h3>You can select 3 to 24 numbers</h3>');
 
     //function
-    $( "#lottery_balls li img" ).unbind().on( "click", function(e) {
-      if( ($("#selected_numbers li").length) < numberOfSelection){
-        var altText = e.target.alt;
-        $("#selected_numbers").append('<li>'+altText+'</li>');
-      }else {
-        alert("Selection full")
-      }
-
-    });
+    LotterySelection();
     break;
   
   case "Perm 3":
@@ -72,15 +51,7 @@ switch (lottery_type) {
     $("#lottery_selected_text").prepend(' <h3>You can select 4 to 17 numbers</h3>');
 
     //function
-    $( "#lottery_balls li img" ).unbind().on( "click", function(e) {
-      if( ($("#selected_numbers li").length) < numberOfSelection){
-        var altText = e.target.alt;
-        $("#selected_numbers").append('<li>'+altText+'</li>');
-      }else {
-        alert("Selection full")
-      }
-
-    });
+    LotterySelection();
   break;
 
   case "Perm 4":
@@ -90,15 +61,7 @@ switch (lottery_type) {
     $("#lottery_selected_text").prepend(' <h3>You can select 5 to 13 numbers</h3>');
 
     //function
-    $( "#lottery_balls li img" ).unbind().on( "click", function(e) {
-      if( ($("#selected_numbers li").length) < numberOfSelection){
-        var altText = e.target.alt;
-        $("#selected_numbers").append('<li>'+altText+'</li>');
-      }else {
-        alert("Selection full")
-      }
-
-    });
+    LotterySelection();
   break;
 
   case "Perm 5":
@@ -108,13 +71,15 @@ switch (lottery_type) {
     $("#lottery_selected_text").prepend(' <h3>You can select 6 to 12 numbers</h3>');
 
     //function
+    LotterySelection();
+  break;
+
+  case "":
+    $("#selected_numbers li").remove();
+    $("#lottery_selected_text h3").remove();
+    //function
     $( "#lottery_balls li img" ).unbind().on( "click", function(e) {
-      if( ($("#selected_numbers li").length) < numberOfSelection){
-        var altText = e.target.alt;
-        $("#selected_numbers").append('<li>'+altText+'</li>');
-      }else {
-        alert("Selection full")
-      }
+        alert("Select lottery type");
 
     });
   break;
@@ -122,14 +87,45 @@ switch (lottery_type) {
 
   default:
     alert("Select lottery type");
+    $("#selected_numbers li").remove();
     $("#lottery_selected_text h3").remove();
     $("#selected_numbers").prepend(' <li>?</li><li>?</li><li>?</li>');
     break;
 
   
 }
+//Lottery Selection Function
+function LotterySelection() {
+  $( "#lottery_balls li img" ).unbind().on( "click", function(e) {
+    if( ($("#selected_numbers li").length) < numberOfSelection){
+      var selectedItems = [];
+      $("#selected_numbers li").each(function() { selectedItems.push($(this).text()) });
+      var altText = e.target.alt;
+      if (selectedItems.includes(altText)){
+        var selectedValueNumber = selectedItems.indexOf(altText);
+        $("#selected_numbers li")[selectedValueNumber].remove();
+      }else {
+      $("#selected_numbers").append('<li>'+altText+'</li>');
+      };
+    }else if(($("#selected_numbers li").length) == numberOfSelection) {
+      var selectedItems = [];
+      $("#selected_numbers li").each(function() { selectedItems.push($(this).text()) });
+      var altText = e.target.alt;
+      if (selectedItems.includes(altText)){
+        var selectedValueNumber = selectedItems.indexOf(altText);
+        $("#selected_numbers li")[selectedValueNumber].remove();
+      }else {
+        alert("Selection full");
+      }
+    }else {
+      alert("Selection full")
+    }
 
+    
 
+  });
+}
   
 
 });
+
